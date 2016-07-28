fcbda1dcd5b495e780029ed390a86a65be18b6e7
clickhostcl <javieralexisparra@clickhost.cl>
2016-07-28 13:53:44 -0400

    Apuntes

---
+++ Apuntes/Clase 1.php
@@ -0,0 +1,13 @@
+Instalación de Composer y Laravel 5.1
+======================================
+
+1) Como intalar composer desde la terminal
+- curl -sS https://getcomposer.org/installer | php
+- mv composer.phar /usr/local/bin/composer
+
+2) Como crear un proyecto laravel desde composer
+- composer create-project laravel/laravel first_streps "5.1.*"
+
+3) Como levantar el servidor de laravel
+- ir ala carpeta del proyecto
+- Comando: php artisan serve
---
+++ Apuntes/Clase 2.php
@@ -0,0 +1,9 @@
+Flujo de trabajo en el desarrollo con Laravel
+============================================
+
+1) Comando para ver las pruebas con la consola
+- Comando: vendor/bin/phpunit
+
+2) Comando para crear archivos y otros
+- Comando: php artisan make:test NotesTest (Crear Archivo)
+- Comando: php artisan make:model Note (Crear Clase)
